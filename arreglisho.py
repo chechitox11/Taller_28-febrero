b631c84f19bfc495a678c5b31fa12b9512bd4f4b
Ubuntu <ubuntu@ip-172-31-31-54.ec2.internal>
2023-03-02 21:02:15 +0000

    b

--- arreglisho.py
+++ arreglisho.py
@@ -1,10 +1,6 @@
-def encontrar_minimo(arr):
-    minimo = arr[0]
-    for num in arr:
-        if num < minimo:
-            minimo = num
-    return minimo
-    
-a = [2,3,5,7,8,1]
-
-print("El numero menor del arreglo es: ", encontrar_minimo(a))
+def funcion(arr):
+    mini = arr[0]
+    for x in range (1,len(arr)):
+        if arr[x] < mini:
+            mini = arr[x]
+    return mini
